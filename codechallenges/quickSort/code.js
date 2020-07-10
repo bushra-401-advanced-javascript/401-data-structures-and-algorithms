3b20cfc5a770f72b5cbe330dea0684bcd5e16eae
Bushra-B <bushraxbil@gmail.com>
2020-07-10 23:39:12 +0300

    CC28, code

--- codechallenges/quickSort/code.js
+++ codechallenges/quickSort/code.js
@@ -1,8 +1,36 @@
 'use strict';
 
-function QuickSort(arr) {
+function QuickSort(arr, left, right) {
+  if (left < right) {
+    let position = Partition(arr, left, right);
 
+    QuickSort(arr, left, position - 1);
 
+    QuickSort(arr, position + 1, right);
+  }
+}
+
+function Partition(arr, left, right) {
+  let povit = arr[right];
+  let low = left - 1;
+
+  for(let i = left; i < right; i++) {
+
+    if (arr[i] <= povit) {
+      low++;
+      Swap(arr, i ,low);
+    }
+
+    Swap(arr, right, low + 1);
+    return low + 1;
+  }
+}
+
+function Swap(arr, i, low) {
+  let temp;
+  temp = arr[i];
+  arr[i] = arr[low];
+  arr[low] = temp;
 }
 
 module.exports = QuickSort;
