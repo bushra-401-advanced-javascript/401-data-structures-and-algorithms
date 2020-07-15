1fd0bf1425a7dd88ad0f3c044600173fcf944150
Bushra-B <bushraxbil@gmail.com>
2020-07-15 18:02:23 +0300

    CC 31 - code

--- codechallenges/repeatedWord/repeated-word.js
+++ codechallenges/repeatedWord/repeated-word.js
@@ -1,77 +1,31 @@
-'use strict'
+'use strict';
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-/////////////////////
+const Hashtable = require('../hashtable/hashtable');
+const Hashmap = Hashtable.hashmap;
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-  prepend(value) {
-    const node = new Node(value);
-    if (!this.head) {
-      this.head = node;
-    } else {
-      node.next = this.head;
-      this.head = node;
+
+function repeatedWords(str){
+  let hashmap = new Hashmap(1000);
+  let words = str.split(/\W+/g);
+  let repeatedWord = '';
+  for (let i = 0; i < words.length; i++) {
+    if (!hashmap.contains(words[i].toLowerCase()).includes(false)) {
+      repeatedWord = words[i];
+      break;
+    }
+    else {
+      hashmap.add(words[i].toLowerCase(), 'String');
     }
   }
-}
-//////////////
-
-class Hashmap {
-  constructor(size) {
-    this.size = size;
-    this.storage = new Array(size);
-  }
-  hash(key) {
-    const sumCharCode = key.split('').reduce((acc, char) => {
-      return acc + char.charCodeAt(0);
-    }, 0);
-    const hashKey = (sumCharCode * 19) % this.size;
-    return hashKey;
-  }
-  add(key, value) {
-    const hash = this.hash(key);
-    if (!this.storage[hash]) {
-      const ll = new LinkedList();
-      ll.prepend({ [key]: value });
-      this.storage[hash] = ll;
-    } else {
-      this.storage[hash].prepend({ [key]: value });
-    }
-  }
-
-  get(key) {
-    const hash = this.hash(key);
-    if (this.storage[hash]) {
-      return this.storage[hash];
-    } else {
-      return 'no value for this key';
-    }
-
-  }
-  contain(key) {
-    const hash = this.hash(key);
-    if (this.storage[hash]) {
-      return true;
-    } else {
-      return false;
-    }
-
-  }
+  return repeatedWord || 'Unique String: No word was repeated..';
 }
 
-function repeatedWords(words){
-  let hashmap = new Hashmap(97);
-  let splitted = words.split(' '); //check if there is ,;?!
-  splitted.forEach(word=> {
-  let hashedWord = hashmap.hash(word);
-    hashmap.add(word);
-  })
-}
+/////////////////////////////
+
+const str1 = 'Once upon a time, there was a brave princess who...';
+const str2 = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
+const str3 = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...';
+
+console.log('Repeated Word Is: ', repeatedWords(str1), '\n');
+console.log('Repeated Word Is: ', repeatedWords(str2), '\n');
+console.log('Repeated Word Is: ', repeatedWords(str3), '\n');
